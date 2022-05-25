d2ff5444eef5dcdc9a856b8be3caf3767767277d
farbs03 <cgeraldfarber@icloud.com>
2022-05-25 15:43:49 -0400

    basic ui completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './index.css';
 
-import {ArrowCircleRightIcon, PlusIcon} from "@heroicons/react/outline"
+import {ArrowCircleRightIcon, ArrowLeftIcon, ChevronLeftIcon, PlusIcon} from "@heroicons/react/outline"
 import Modal from "./components/Modal"
 import axios from 'axios';
 
@@ -99,7 +99,15 @@ function App() {
                   onClick={uploadImage}
                 >
                     <p className='text-base'>Extract Text</p>
-                    <ArrowCircleRightIcon className='w-6 h-6 group-hover:translate-x-1 transition duration-100 ease-in' />
+                    {!loading ? 
+                      <ArrowCircleRightIcon className='w-6 h-6 group-hover:translate-x-1 transition duration-100 ease-in' />
+                      :
+                      <svg class="animate-spin h-6 w-6 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                      </svg>
+                    }
+                    
                 </button>
 
                 <p>or</p>
@@ -116,6 +124,13 @@ function App() {
             </>
             :
             <>
+              <button
+                onClick={imageResetHandler}
+                className='mr-auto flex items-center gap-2 text-blue-500 font-semibold'
+              >
+                <ChevronLeftIcon className='w-6 h-6' />
+                <p>Go Back</p>
+              </button>
               {textFromImage.split(/\r?\n/).map((line) => (
                 <p>{line}</p>
               ))}
